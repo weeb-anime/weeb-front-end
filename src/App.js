5a3bf8d1358f83e696d6c292acf8a1697c48fe08
Rupinder Hayer <ruphayer@gmail.com>
2021-10-04 16:12:18 -0700

    added form feature and rendering of searched query

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 
 import './App.css';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import AnimeForm from './components/AnimeForm';
+import axios from "axios";
+import SuggestAnime from './components/SuggestAnime';
 
-function App() {
-  return (
-    <div>
- <h1>WEEB is here!!!!!</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      anime: []
+    }
+  }
+  getAnimeList = async (animeInfo) => {
+    const animeURL = `https://api.jikan.moe/v3/search/anime?q=${animeInfo}`
+
+    let animeResponse = await axios.get(animeURL)
+    let animeData = animeResponse.data
+    console.log(animeData.results)
+    this.setState({
+      anime: animeData.results
+    })
+  }
+  render(){
+    return(
+      <>
+      <h1>hell yeah baby</h1>
+      <AnimeForm getAnimeList={this.getAnimeList}/>
+      <SuggestAnime xxx={this.state.anime}/>
+      </>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/AnimeForm.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
+export default class AnimeForm extends React.Component{
+
+  handleSubmit =(event)=>{
+    event.preventDefault();
+    const animeInfo = event.target.anime.value;
+    console.log(animeInfo);
+    this.props.getAnimeList(animeInfo);
+  }
+
+  render(){
+    return(
+      <Form onSubmit ={this.handleSubmit} >
+        <Form.Group controlId = "anime">
+          <Form.Control type = 'text' placeholder='Search'>
+          </Form.Control>
+        </Form.Group>
+        <Button variant='primary' type='submit'>GO WEEBS!!!</Button>
+      </Form>
+    
+    )
+  }
+}
