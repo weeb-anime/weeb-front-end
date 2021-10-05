119e90be799bd429df51bd868d270286ae0e4d69
Tripp Lucas <tripppdx@gmail.com>
2021-10-05 14:45:06 -0700

    Fix auth0 login

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SuggestAnime from './components/SuggestAnime';
 import Profile from './components/Profile';
 import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 import { withAuth0 } from '@auth0/auth0-react';
 
 class App extends React.Component {
@@ -18,24 +19,27 @@ class App extends React.Component {
     };
   }
 
-  async componentDidMount() {
-    if (this.props.auth0.isAuthenticated) {
-      const res = await this.props.auth0.getIdTokenClaims();
+  // async componentDidMount() {
+  //   console.log(
+  //     'ComponentDidMount isAuthenticated:',
+  //     this.props.auth0.isAuthenticated
+  //   );
+  //   if (this.props.auth0.isAuthenticated) {
+  //     const res = await this.props.auth0.getIdTokenClaims();
 
-      const jwt = res.__raw;
+  //     const jwt = res.__raw;
 
-      const config = {
-        headers: { Authorization: `Bearer ${jwt}` },
-        method: 'get',
-        baseURL: process.env.REACT_APP_API_URL,
-        URL: '/anime',
-      };
-      const animeResponse = await axios(config);
-      this.setState({ anime: animeResponse.data });
-      this.setState({ user: this.props.auth0.user });
-      this.props.handleAuth(this.user);
-    }
-  }
+  //     const config = {
+  //       headers: { Authorization: `Bearer ${jwt}` },
+  //       method: 'get',
+  //       baseURL: process.env.REACT_APP_API_URL,
+  //       URL: '/anime',
+  //     };
+  //     const animeResponse = await axios(config);
+  //     this.setState({ anime: animeResponse.data });
+  //     this.setState({ user: this.props.auth0.user });
+  //   }
+  // }
 
   getAnimeList = async animeInfo => {
     const animeURL = `${process.env.REACT_APP_API_URL}/anime?searchQuery=${animeInfo}`;
@@ -48,9 +52,13 @@ class App extends React.Component {
     });
   };
   render() {
+    console.log('RENDER isAuthenticated:', this.props.auth0.isAuthenticated);
+    console.log('USER:', this.props.auth0.user);
     return (
       <>
         <LoginButton />
+        <LogoutButton />
+        {/* {this.props.auth0.isAuthenticated && <h1>hell yeah baby</h1>} */}
         {/* <Router>
           <Switch>
             <Route exact path="/">
--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -4,7 +4,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 function LoginButton() {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
-  console.log('LOGINBUTTON');
+  // console.log('LOGINBUTTON');
   return (
     !isAuthenticated && <Button onClick={loginWithRedirect}>Log in</Button>
   );
