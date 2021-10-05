8c66556f6dbf42186fd401041ac3bff490e8a60e
Rupinder Hayer <ruphayer@gmail.com>
2021-10-05 10:52:49 -0700

    added profile page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import AnimeForm from './components/AnimeForm';
 import axios from "axios";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import SuggestAnime from './components/SuggestAnime';
+import Profile from './components/Profile'
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state={
-      anime: []
+      anime: [],
+      user: null
     }
   }
   getAnimeList = async (animeInfo) => {
@@ -26,9 +29,18 @@ class App extends React.Component {
   render(){
     return(
       <>
+      <Router>
+        <Switch>
+          <Route path='/profile'>
+            <Profile/>
+          </Route>
+          <Route>
       <h1>hell yeah baby</h1>
       <AnimeForm getAnimeList={this.getAnimeList}/>
       <SuggestAnime xxx={this.state.anime}/>
+      </Route>
+      </Switch>
+      </Router>
       </>
     )
   }
---
+++ src/components/Profile.js
@@ -0,0 +1,9 @@
+import {Component} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+
+export default class Profile extends Component {
+  render(){
+    <Button type="button" class="btn btn-primary">Log In</Button>
+  }
+}
