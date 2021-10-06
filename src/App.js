9045230292b66435902455e30efb415116c2497a
Tripp Lucas <tripppdx@gmail.com>
2021-10-05 17:29:48 -0700

    Add profile feature

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SuggestAnime from './components/SuggestAnime';
 import Profile from './components/Profile';
 import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
 import { withAuth0 } from '@auth0/auth0-react';
+import Header from './components/Header';
 
 class App extends React.Component {
   constructor(props) {
@@ -19,27 +19,27 @@ class App extends React.Component {
     };
   }
 
-  // async componentDidMount() {
-  //   console.log(
-  //     'ComponentDidMount isAuthenticated:',
-  //     this.props.auth0.isAuthenticated
-  //   );
-  //   if (this.props.auth0.isAuthenticated) {
-  //     const res = await this.props.auth0.getIdTokenClaims();
+  async componentDidMount() {
+    console.log(
+      'ComponentDidMount isAuthenticated:',
+      this.props.auth0.isAuthenticated
+    );
+    if (this.props.auth0.isAuthenticated) {
+      const res = await this.props.auth0.getIdTokenClaims();
 
-  //     const jwt = res.__raw;
+      const jwt = res.__raw;
 
-  //     const config = {
-  //       headers: { Authorization: `Bearer ${jwt}` },
-  //       method: 'get',
-  //       baseURL: process.env.REACT_APP_API_URL,
-  //       URL: '/anime',
-  //     };
-  //     const animeResponse = await axios(config);
-  //     this.setState({ anime: animeResponse.data });
-  //     this.setState({ user: this.props.auth0.user });
-  //   }
-  // }
+      const config = {
+        headers: { Authorization: `Bearer ${jwt}` },
+        method: 'get',
+        baseURL: process.env.REACT_APP_API_URL,
+        URL: '/anime',
+      };
+      const animeResponse = await axios(config);
+      this.setState({ anime: animeResponse.data });
+      this.setState({ user: this.props.auth0.user });
+    }
+  }
 
   getAnimeList = async animeInfo => {
     const animeURL = `${process.env.REACT_APP_API_URL}/anime?searchQuery=${animeInfo}`;
@@ -53,19 +53,15 @@ class App extends React.Component {
   };
   render() {
     console.log('RENDER isAuthenticated:', this.props.auth0.isAuthenticated);
-    console.log('USER:', this.props.auth0.user);
+    // console.log('USER:', this.props.auth0.user);
     return (
       <>
-        <LoginButton />
-        <LogoutButton />
-        {/* {this.props.auth0.isAuthenticated && <h1>hell yeah baby</h1>} */}
-        {/* <Router>
+        <Router>
           <Switch>
             <Route exact path="/">
-
               {this.props.auth0.isAuthenticated ? (
                 <>
-                  <h1>hell yeah baby</h1>
+                  <Header />
                   <AnimeForm getAnimeList={this.getAnimeList} />
                   <SuggestAnime xxx={this.state.anime} />
                 </>
@@ -75,11 +71,11 @@ class App extends React.Component {
                 </>
               )}
             </Route>
-            <Route path="/profile">
+            <Route path="/Profile">
               <Profile user={this.state.user} />
             </Route>
           </Switch>
-        </Router> */}
+        </Router>
       </>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import NavItem from 'react-bootstrap/NavItem';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import LogoutButton from './LogoutButton';
+
+class Header extends Component {
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <NavItem>
+            <Link to="/profile" classname="nav-link">
+              Profile
+            </Link>
+          </NavItem>
+          <NavItem>
+            <LogoutButton />
+          </NavItem>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default Header;
