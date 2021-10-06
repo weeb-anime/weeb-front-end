1a797e9a5fa3c1f826ddb77c8c46198bf801c112
Rupinder Hayer <ruphayer@gmail.com>
2021-10-05 18:55:17 -0700

    added filter for anime PG13 PG

--- src/components/SuggestAnime.js
+++ src/components/SuggestAnime.js
@@ -3,39 +3,38 @@ import { Card } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 
-export default class SuggestAnime extends React.Component{
-  
+export default class SuggestAnime extends React.Component {
+
   render(
-  ){
+  ) {
     console.log(this.props.xxx)
-    return(
+    return (
       <>
-      <Container>
-        <Card>
-          <Card.Header>Anime List</Card.Header>
-          <Row xs={1} sm ={1} md={2} lg={3}>
-          {this.props.xxx.map((anime, index)=>(
-            <SingleAnime anime={anime} key={index}/>
-          ))}
+        <Container>
+          <Row xs={1} sm={1} md={2} lg={3}>
+            {this.props.xxx.map((anime, index) => (
+              <SingleAnime anime={anime} key={index} />
+            ))}
           </Row>
-        </Card>
-      </Container>
+        </Container>
       </>
     )
   }
 }
 
-class SingleAnime extends React.Component{
-render(){
-  return(
-    <>
-    <Card.Body>
-      <Card.Text as ='h4'>
-        <p>{this.props.anime.title}</p>
-      </Card.Text>
-      <Card.Img variant = 'bottom' src ={this.props.anime.image_url}/>
-    </Card.Body>
-    </>
-  )
-}
+class SingleAnime extends React.Component {
+  render() {
+    return (
+      <>
+        {this.props.anime.rated === ("PG-13" || "PG" || "G") && <>  <Card>
+          <Card.Body><Card.Text as='h4'>
+            <p>{this.props.anime.title}</p>
+          </Card.Text>
+            <Card.Img variant='bottom' src={this.props.anime.image_url} />
+          </Card.Body>
+        </Card>
+        </>}
+      </>
+    )
+  }
 }
