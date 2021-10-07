3d692593f9870d36682be93684057dbae31b2fc2
Rupinder Hayer <ruphayer@gmail.com>
2021-10-07 12:07:18 -0700

    added alert on add button, moved add button to modal

--- src/components/AnimeModal.js
+++ src/components/AnimeModal.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-
+import { Alert } from 'react-bootstrap';
 export default class AnimeModal extends Component {
+
+  onAdd = () => this.props.handleAdd(this.props.anime);
+  
+
   render() {
     return (
-      <Modal show={this.props.show} onHide={this.props.hide}>
+      <Modal show={this.props.show} onHide={()=>{this.props.hide(); this.props.hideAlert()}}>
         <Modal.Header closeButton>
           <Modal.Title>{this.props.anime.title}</Modal.Title>
         </Modal.Header>
@@ -30,9 +34,19 @@ export default class AnimeModal extends Component {
           </Card>
         </Modal.Body>{' '}
         <Modal.Footer>
-          <Button variant="primary" onClick={this.props.hide}>
+        <Button onClick={this.onAdd} >Add</Button>
+        {this.props.showAlert && (
+          <Alert variant="success">
+          <Alert.Heading>Added to your watchlist</Alert.Heading>
+          <p>
+           To check your watchlist. Please go to    <Alert.Link href="/profile" >profile</Alert.Link> page
+          </p>
+        </Alert>
+        )}
+          <Button variant="primary" onClick={()=>{this.props.hide(); this.props.hideAlert()}}>
             Close
           </Button>
+   
         </Modal.Footer>
       </Modal>
     );
