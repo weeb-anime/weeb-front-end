20831356554f0cd96873e0323b0929740799c0fc
George Mead <gmeadiv@gmail.com>
2021-10-07 10:41:48 -0700

    adds img and changes font on about.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,13 @@ export default class About extends Component {
   render() {
     return (
       <>
-        <h1>Hi from About</h1>
+        <body className="font-link" style={
+          {
+            backgroundColor: '#404040',
+            color: '#C0C0C0'
+          }}>
+        <h1>About the Weeb Team</h1>
+        </body>
       </>
     );
   }
